84815189449936e6f348ab44f4c156eb205e59a7
Selin Karagullu <selin.karagullu@gmail.com>
2026-09-03 07:27:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import http from 'node:http'
+import fs from 'node:fs/promises'
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     console.log(req.url)
   if (req.url === '/api/projects') {
+   const data = await fs.readFile('data/projects.json')
     res.end('projects')
 }  else {
     res.end('Not found')
