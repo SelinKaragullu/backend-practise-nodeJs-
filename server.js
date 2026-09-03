431e3a1e4e77096b878cdc2f1d85f1c574ee9bf2
Selin Karagullu <selin.karagullu@gmail.com>
2026-09-03 07:17:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ import http from 'node:http'
 
 const server = http.createServer((req, res) => {
     console.log(req.url)
-    res.end('Hello')
+  if (req.url === '/api/projects') {
+    res.end('projects')
+}  else {
+    res.end('Not found')
+}
 })
 
 server.listen(8000, () => {
